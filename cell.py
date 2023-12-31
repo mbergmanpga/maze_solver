402664b7d7f6895d7741272f1db4ab4f966480f5
mbergmanpga <mbergmanpga@gmail.com>
2023-12-31 08:27:00 -0500

    completed break_the_walls

--- cell.py
+++ cell.py
@@ -14,7 +14,10 @@ class Cell:
         self._y2 = None 
         self._win = win
 
+
     def draw(self, x1, y1, x2, y2, fill_color = "black"):
+        if self._win is None:
+            return
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
@@ -46,6 +49,8 @@ class Cell:
 
 
     def draw_move(self, to_cell, undo=False):
+        if self._win is None:
+            return
         x_mid = (self._x1 + self._x2)/2
         y_mid = (self._y1 + self._y2)/2
         to_x_mid = (to_cell._x2 + to_cell._x1)/2
@@ -81,4 +86,7 @@ class Cell:
             self._win.draw_line(line, fill_color)
             line = Line(Point(to_x_mid, to_y_mid), Point(to_x_mid, to_cell._y1))
             self._win.draw_line(line, fill_color)
+
+
+
 
